d0e5e6c2affcc7ac504bb423f73c3c5533fdfa2d
Cristian Moncho <cristian.moncho@diagram.es>
2018-05-08 13:42:53 +0200

    [IMP] base: Improve QWeb memory usage

--- openerp/addons/base/ir/ir_qweb.py
+++ openerp/addons/base/ir/ir_qweb.py
@@ -314,20 +314,22 @@ class QWeb(orm.AbstractModel):
         # generated_attributes: generated attributes
         # qwebcontext: values
         # inner: optional innerXml
+        g_inner = cStringIO.StringIO()
         if inner:
-            g_inner = inner.encode('utf-8') if isinstance(inner, unicode) else inner
+            g_inner.write(inner.encode('utf-8') if isinstance(inner, unicode) else inner)
         else:
-            g_inner = [] if element.text is None else [element.text.encode('utf-8')]
+            if element.text is not None:
+                g_inner.write(element.text.encode('utf-8'))
             for current_node in element.iterchildren(tag=etree.Element):
                 try:
-                    g_inner.append(self.render_node(current_node, qwebcontext))
+                    g_inner.write(self.render_node(current_node, qwebcontext))
                 except QWebException:
                     raise
                 except Exception:
                     template = qwebcontext.get('__template__')
                     raise_qweb_exception(message="Could not render element %r" % element.tag, node=element, template=template)
         name = unicode(element.tag)
-        inner = "".join(g_inner)
+        inner = g_inner.getvalue()
         trim = template_attributes.get("trim", 0)
         if trim == 0:
             pass
