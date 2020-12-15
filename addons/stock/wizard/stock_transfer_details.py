f801ff3a24ccd1a9901e79e5afaf4558dbd290da
Zambu <jose@aurestic.es>
2020-12-15 14:15:20 +0100

    [STOCK][IMP] Modulate pack operation data to new method for inherit

--- addons/stock/wizard/stock_transfer_details.py
+++ addons/stock/wizard/stock_transfer_details.py
@@ -62,7 +62,7 @@ class stock_transfer_details(models.TransientModel):
                 'sourceloc_id': op.location_id.id,
                 'destinationloc_id': op.location_dest_id.id,
                 'result_package_id': op.result_package_id.id,
-                'date': op.date, 
+                'date': op.date,
                 'owner_id': op.owner_id.id,
             }
             if op.product_id:
@@ -73,6 +73,21 @@ class stock_transfer_details(models.TransientModel):
         res.update(packop_ids=packs)
         return res
 
+    @api.model
+    def get_pack_operation_data(self, prod):
+        return {
+            'product_id': prod.product_id.id,
+            'product_uom_id': prod.product_uom_id.id,
+            'product_qty': prod.quantity,
+            'package_id': prod.package_id.id,
+            'lot_id': prod.lot_id.id,
+            'location_id': prod.sourceloc_id.id,
+            'location_dest_id': prod.destinationloc_id.id,
+            'result_package_id': prod.result_package_id.id,
+            'date': prod.date if prod.date else datetime.now(),
+            'owner_id': prod.owner_id.id,
+        }
+
     @api.one
     def do_detailed_transfer(self):
         if self.picking_id.state not in ['assigned', 'partially_available']:
@@ -82,18 +97,7 @@ class stock_transfer_details(models.TransientModel):
         # Create new and update existing pack operations
         for lstits in [self.item_ids, self.packop_ids]:
             for prod in lstits:
-                pack_datas = {
-                    'product_id': prod.product_id.id,
-                    'product_uom_id': prod.product_uom_id.id,
-                    'product_qty': prod.quantity,
-                    'package_id': prod.package_id.id,
-                    'lot_id': prod.lot_id.id,
-                    'location_id': prod.sourceloc_id.id,
-                    'location_dest_id': prod.destinationloc_id.id,
-                    'result_package_id': prod.result_package_id.id,
-                    'date': prod.date if prod.date else datetime.now(),
-                    'owner_id': prod.owner_id.id,
-                }
+                pack_datas = self.get_pack_operation_data(prod)
                 if prod.packop_id:
                     prod.packop_id.with_context(no_recompute=True).write(pack_datas)
                     processed_ids.append(prod.packop_id.id)
