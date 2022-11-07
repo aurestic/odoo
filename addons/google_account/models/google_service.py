cf6f7d70354bd9d5f201a64c78ad953b7fd13246
Jose Zambudio Bernabeu <zamberjo@gmail.com>
2022-11-07 07:15:12 +0100

    [BACKPORT] fetchmail_gmail (#16)
    [BACKPORT] fetchmail_outlook
    [BACKPORT] google_account
    [BACKPORT] google_gmail
    [BACKPORT] microsoft_outlook

--- addons/google_account/models/google_service.py
+++ addons/google_account/models/google_service.py
@@ -25,6 +25,7 @@ GOOGLE_API_BASE_URL = 'https://www.googleapis.com'
 # FIXME : this needs to become an AbstractModel, to be inhereted by google_calendar_service and google_drive_service
 class GoogleService(models.TransientModel):
     _name = 'google.service'
+    _description = 'Google Service'
 
     @api.model
     def generate_refresh_token(self, service, authorization_code):
@@ -119,6 +120,42 @@ class GoogleService(models.TransientModel):
             error_msg = _("Something went wrong during your token generation. Maybe your Authorization Code is invalid")
             raise self.env['res.config.settings'].get_config_warning(error_msg)
 
+    @api.model
+    def _get_access_token(self, refresh_token, service, scope):
+        """Fetch the access token thanks to the refresh token."""
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        client_id = get_param('google_%s_client_id' % service, default=False)
+        client_secret = get_param('google_%s_client_secret' % service, default=False)
+
+        if not client_id or not client_secret:
+            raise UserError(_('Google %s is not yet configured.', service.title()))
+
+        if not refresh_token:
+            raise UserError(_('The refresh token for authentication is not set.'))
+
+        try:
+            result = requests.post(
+                GOOGLE_TOKEN_ENDPOINT,
+                data={
+                    'client_id': client_id,
+                    'client_secret': client_secret,
+                    'refresh_token': refresh_token,
+                    'grant_type': 'refresh_token',
+                    'scope': scope,
+                },
+                headers={'Content-type': 'application/x-www-form-urlencoded'},
+                timeout=TIMEOUT,
+            )
+            result.raise_for_status()
+        except requests.HTTPError:
+            raise UserError(
+                _('Something went wrong during the token generation. Please request again an authorization code.')
+            )
+
+        json_result = result.json()
+
+        return json_result.get('access_token'), json_result.get('expires_in')
+
     # FIXME : this method update a field defined in google_calendar module. Since it is used only in that module, maybe it should be moved.
     @api.model
     def _refresh_google_token_json(self, refresh_token, service):  # exchange_AUTHORIZATION vs Token (service = calendar)
@@ -127,7 +164,7 @@ class GoogleService(models.TransientModel):
         client_secret = get_param('google_%s_client_secret' % (service,), default=False)
 
         if not client_id or not client_secret:
-            raise UserError(_("The account for the Google service '%s' is not configured") % service)
+            raise UserError(_("The account for the Google service '%s' is not configured.") % service)
 
         headers = {"content-type": "application/x-www-form-urlencoded"}
         data = {
