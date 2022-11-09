f72704d3c51992a9f90371a1f1016da8d41ee865
Jose Zambudio Bernabeu <jose@aurestic.es>
2022-11-09 10:55:43 +0100

    [FIX] microsoft_outlook missing (set|get)_values

--- addons/microsoft_outlook/models/res_config_settings.py
+++ addons/microsoft_outlook/models/res_config_settings.py
@@ -7,19 +7,17 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    microsoft_outlook_client_identifier = fields.Char(
-        "Outlook Client Id", config_parameter="microsoft_outlook_client_id"
-    )
-    microsoft_outlook_client_secret = fields.Char(
-        "Outlook Client Secret", config_parameter="microsoft_outlook_client_secret"
-    )
+    microsoft_outlook_client_identifier = fields.Char("Outlook Client Id")
+    microsoft_outlook_client_secret = fields.Char("Outlook Client Secret")
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         ICP = self.env["ir.config_parameter"]
         res.update(
-            microsoft_outlook_client_identifier=ICP.get_param("microsoft_outlook_client_id"),
+            microsoft_outlook_client_identifier=ICP.get_param(
+                "microsoft_outlook_client_id"
+            ),
             microsoft_outlook_client_secret=ICP.get_param(
                 "microsoft_outlook_client_secret"
             ),
@@ -29,7 +27,9 @@ class ResConfigSettings(models.TransientModel):
     def set_values(self):
         super(ResConfigSettings, self).set_values()
         ICP = self.env["ir.config_parameter"]
-        ICP.set_param("microsoft_outlook_client_id", self.microsoft_outlook_client_identifier)
+        ICP.set_param(
+            "microsoft_outlook_client_id", self.microsoft_outlook_client_identifier
+        )
         ICP.set_param(
             "microsoft_outlook_client_secret", self.microsoft_outlook_client_secret
         )
