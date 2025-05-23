13045f6dc8308ea5532973f02bdf945571300de0
almumu <almumu@users.noreply.github.com>
2025-05-23 09:28:28 +0200

    sólo aplicar el control de lotes al producir si lo tienen activado en la compañía (#28)

--- addons/mrp/mrp.py
+++ addons/mrp/mrp.py
@@ -1007,7 +1007,11 @@ class mrp_production(osv.osv):
                         break
                     if consume['product_id'] != raw_material_line.product_id.id:
                         continue
-                    if consume['lot_id'] and raw_material_line.restrict_lot_id and consume['lot_id'] != raw_material_line.restrict_lot_id.id:
+                    if (
+                        production.company_id.manufacturing_check_lots and
+                        consume['lot_id'] and
+                        consume['lot_id'] != raw_material_line.restrict_lot_id.id
+                    ):
                         continue
                     consumed_qty = min(remaining_qty, raw_material_line.product_qty)
                     stock_mov_obj.action_consume(cr, uid, [raw_material_line.id], consumed_qty, raw_material_line.location_id.id,
